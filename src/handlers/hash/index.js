effc690ac577f4264bb544d835a2d5ba406e0c6a
EkaterinaKunskaya <kunskaya.ekaterina@gmail.com>
2024-02-03 18:14:51 +0300

    feat: add hash

--- src/handlers/hash/index.js
+++ src/handlers/hash/index.js
@@ -1,3 +1,14 @@
-export const hash = async (filePath) => {
+import { createHash } from 'node:crypto';
+import { resolve } from 'node:path';
+import { createReadStream } from 'node:fs';
 
+export const hash = async (filePath) => {
+    const fileStream = createReadStream(resolve(process.cwd(), filePath));
+	const hash = createHash('SHA256').setEncoding('hex');
+
+	fileStream.pipe(hash);
+	fileStream.on('end', () => {
+		hash.end();
+		console.log(hash.read());
+	});
 };
