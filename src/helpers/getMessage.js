9d687f0b77dbd413605f917c27f70de823e93448
EkaterinaKunskaya <kunskaya.ekaterina@gmail.com>
2024-02-03 13:01:13 +0300

    feat: add getMessage helper

---
+++ src/helpers/getMessage.js
@@ -0,0 +1,24 @@
+import { WELCOME, GOODBYE, CURRENT_DIR, UNKNOWN_OPERATION, ERROR } from '../constants/constants.js';
+
+export const getMessage = (type, arg) => {
+    switch (type) {
+        case WELCOME:
+            console.log(`Welcome to the File Manager, ${arg}!`);
+            break;
+        case GOODBYE:
+            console.log(`Thank you for using File Manager, ${arg}, goodbye!`);
+            break;
+        case CURRENT_DIR:
+            console.log(`You are currently in ${arg}`);
+            break;
+        case UNKNOWN_OPERATION:
+            console.log('Invalid input');
+            break;
+        case ERROR:
+            console.log('Operation failed');
+            break;
+        default:
+            console.log('Oops, something went wrong');
+            console.log(arg);
+    }
+}
