c55ace79783ef98de41fc65c8ca0022d1eeb4fa3
EkaterinaKunskaya <kunskaya.ekaterina@gmail.com>
2024-02-03 13:17:44 +0300

    feat: inplement general logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hi');
+import { createInterface } from 'readline';
+// CONSTANTS
+import {
+    WELCOME,
+    GOODBYE,
+    CURRENT_DIR,
+    ARGUMENT_PREFIX_USERNAME,
+} from './constants/constants.js';
+// HELPERS
+import { getMessage } from './helpers/getMessage.js';
+import { handleCommand } from './helpers/handleCommand.js';
+
+
+const username = process.argv.find(argument => argument.startsWith(ARGUMENT_PREFIX_USERNAME)).replace(ARGUMENT_PREFIX_USERNAME, '');
+
+const readlineInterface = createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+getMessage(WELCOME, username);
+getMessage(CURRENT_DIR, process.cwd());
+readlineInterface.on('line', handleCommand);
+
+process.on('exit', () => {
+    getMessage(GOODBYE, username);
+    readlineInterface.close();
+});
